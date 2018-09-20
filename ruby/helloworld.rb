1d0afe39182de94691fa81c26c82bf1d227ed494
Jackwhitener <jackwhitener1@gmail.com>
2018-09-20 14:03:28 -0400

    Adds third counter, counting out every odd number.

--- ruby/helloworld.rb
+++ ruby/helloworld.rb
@@ -1,12 +1,22 @@
 puts "Hello world!"
 
-f = 1
+# f = 1
+# 100.times do
+# 	puts f
+# 	f += 1
+# end
+# g = 1
+# while g < 101
+# 	puts g
+# 	g += 1
+# end
+h = 1
+
 100.times do
-	puts f
-	f += 1
-end
-g = 1
-while g < 101
-	puts g
-	g += 1
+	if h % 2 == 0
+		puts h
+	else
+		puts "Odd"
+	end
+	h += 1
 end
