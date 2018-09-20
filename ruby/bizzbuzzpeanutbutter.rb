c95647ace800ff74e9262a7da87c562cff429e2d
Jackwhitener <jackwhitener1@gmail.com>
2018-09-20 15:59:04 -0400

    makes bbpb more ergonomic

--- ruby/bizzbuzzpeanutbutter.rb
+++ ruby/bizzbuzzpeanutbutter.rb
@@ -1,13 +1,16 @@
 peanutbuttercounter = 0
 while peanutbuttercounter < 100
+
 	peanutbuttercounter += 1
 	print "#{peanutbuttercounter} "
-	puts " "
+
 	if peanutbuttercounter % 3 == 0
 		print "peanut"
 	end
 	if peanutbuttercounter % 5 == 0
 		print "butter"
 	end
+
 	puts " "
+
 end
