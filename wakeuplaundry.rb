6572a510ff40b67a24d2ff0b414dd4963fecd0d0
Jackwhitener <jackwhitener1@gmail.com>
2018-09-20 09:24:17 -0400

    Cleaned up wakeuplandry.rb, got a bit spread out in transfer.

--- wakeuplaundry.rb
+++ wakeuplaundry.rb
@@ -1,73 +1,43 @@
 #Let's get this biz started
 
 def wakeup(woke)
-
   if woke >=3
-
-
     sample :elec_beep
   else
-
     sample :elec_bell, rate: woke
-
     sleep(1)
     wakeup(woke+1)
-
   end
 end
 
-
 def shower(clean)
-
   play(clean)
-
   sleep(1)
-
 end
-
 
 def brushteeth(toothpaste)
-
   sample toothpaste
-
   sleep(1)
-
 end
-
 
 def getdressed()
-
   play(57)
-
 end
 
-
 def isweekday()
-
   1 == [1,2].choose
-
   sample :elec_bong
 end
 
-
 end
 def gotoschool
-
   sample :elec_pop
-
 end
 
-
 4.times do
-
-  wakeup(5)
-
+  wakeup(5)\
   shower(50)
-
   brushteeth(1)
-
   getdressed()
-
   gotoschool if isweekday
-
 end
