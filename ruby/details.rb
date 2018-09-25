62bd67fef9015fb88a1c16407f743cf2feed7892
Jackwhitener <jackwhitener1@gmail.com>
2018-09-25 11:08:46 -0400

    small upd8 to details

--- ruby/details.rb
+++ ruby/details.rb
@@ -1,4 +1,4 @@
-def func
+def func()
 puts "What's your name?"
 n = gets.chomp
 puts "What street do you live on?"
@@ -7,4 +7,4 @@ puts "What town/city do you live in?"
 c = gets.chomp
 puts "You told me your name is #{n}, and that you live on #{s}, in #{c}."
 end
-func
+func()
