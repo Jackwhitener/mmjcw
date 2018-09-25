41e6b9043e26894620b57626863b3620fd2c8d09
Jackwhitener <jackwhitener1@gmail.com>
2018-09-25 11:04:45 -0400

    Adds stuff, bizzbuzz, and details.rb.

---
+++ ruby/bizzbuzz.rb
@@ -0,0 +1,16 @@
+applesaucecounter = 0
+while applesaucecounter < 100
+
+	applesaucecounter += 1
+	print "#{applesaucecounter} "
+
+	if applesaucecounter % 3 == 0
+		print "apple"
+	end
+	if applesaucecounter % 5 == 0
+		print "sauce"
+	end
+
+	puts " "
+
+end
---
+++ ruby/details.rb
@@ -0,0 +1,10 @@
+def func
+puts "What's your name?"
+n = gets.chomp
+puts "What street do you live on?"
+s = gets.chomp
+puts "What town/city do you live in?"
+c = gets.chomp
+puts "You told me your name is #{n}, and that you live on #{s}, in #{c}."
+end
+func
