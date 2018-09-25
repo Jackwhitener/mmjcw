90c6c6d66ff957a9b614b4a6ccbc5a228dbfaf44
Jackwhitener <jackwhitener1@gmail.com>
2018-09-25 11:10:11 -0400

    small upd8 to details

--- ruby/details.rb
+++ ruby/details.rb
@@ -1,10 +1,11 @@
 def func()
-puts "What's your name?"
-n = gets.chomp
-puts "What street do you live on?"
-s = gets.chomp
-puts "What town/city do you live in?"
-c = gets.chomp
-puts "You told me your name is #{n}, and that you live on #{s}, in #{c}."
+	puts "What's your name?"
+	n = gets.chomp
+	puts "What street do you live on?"
+	s = gets.chomp
+	puts "What town/city do you live in?"
+	c = gets.chomp
+	puts "You told me your name is #{n}, and that you live on #{s}, in #{c}."
 end
+
 func()
